a179ff1ff85f88c4958b6c172dbad0ac802bc8fc
yereong <lily030624@gmail.com>
2024-10-10 19:07:22 +0900

    [feat] 기본 틀

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Main from './page/Main/Main';
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+
+      <Route path="/" element={<Main />} />
+    </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/page/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main(){
+    return(
+        <div className="Main">
+            메인페이지
+        </div>
+    );
+}
+
+export default Main;
